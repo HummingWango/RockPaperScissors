d0afe232f9d9e3d11280a6a2707fc8fb46eb0401
HummingWango <176415191+HummingWango@users.noreply.github.com>
2025-07-12 10:43:36 -0400

    finished the game

--- rps.js
+++ rps.js
@@ -15,3 +15,45 @@ function getComputerChoice (){
 function getHumanChoice() {
     return prompt("What did you play?");
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore+=1;
+            console.log(`You lost...${computerChoice} beats ${humanChoice}.`);
+            return;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            computerScore+=1;
+            console.log(`You lost...${computerChoice} beats ${humanChoice}.`);
+            return;
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore+=1;
+            console.log(`You lost...${computerChoice} beats ${humanChoice}.`);
+            return;
+        }
+    }
+    if (humanChoice === computerChoice) {
+        console.log(`A tie! ${computerChoice} ties ${humanChoice}`);
+        return;
+    }
+    humanScore +=1;
+    console.log(`You won!!! ${humanChoice} beats ${computerChoice}`);
+    return;
+}
+
+
+function playGame(){
+    for (let i = 0; i<5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    console.log(`Your Score: ${humanScore}     Computer Score: ${computerScore}`)
+}
+
+playGame();
